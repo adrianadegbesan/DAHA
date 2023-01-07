f169dcc827793c7145a805f57453a31ecc5f65a3
adrianadegbesan <adrianadegbesan@yahoo.com>
2023-01-07 09:45:50 -0600

    Initial Commit

---
+++ DAHA/DAHAApp.swift
@@ -0,0 +1,17 @@
+//
+//  DAHAApp.swift
+//  DAHA
+//
+//  Created by Adrian Adegbesan on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct DAHAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
