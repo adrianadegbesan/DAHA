f71d7238ce5a6066ddaca9742d734398aab18691
adrianadegbesan <adrianadegbesan@yahoo.com>
2023-05-20 11:23:03 -0700

    More UI Changes to messaging

--- DAHA/Screens/Messaging/ChatScreen.swift
+++ DAHA/Screens/Messaging/ChatScreen.swift
@@ -57,9 +57,34 @@ struct ChatScreen: View {
                                  }
                                 
                             
-                                Text("Stay safe: Choose to meet in only open, well-lit, public areas and never share personal or sensitive information in the chat.")
+                                (
+                                 Text("STAY SAFE")
+                                        .foregroundColor(Color(hex: deepBlue)) +
+                                 Text(": Choose to meet in only ")
+                                        .foregroundColor(.secondary)  +
+                                 Text("open ")
+                                    .foregroundColor(Color(hex: color_new)) +
+                                 Text(", ")
+                                    .foregroundColor(.secondary) +
+                                 Text("well-lit")
+                                    .foregroundColor(Color(hex: color_new)) +
+                                 Text(", ")
+                                    .foregroundColor(.secondary) +
+                                 Text("public ")
+                                    .foregroundColor(Color(hex: color_new)) +
+                                 Text("areas and never share ")
+                                    .foregroundColor(.secondary) +
+                                 Text("personal")
+                                    .foregroundColor(.red) +
+                                 Text(" or ")
+                                    .foregroundColor(.secondary) +
+                                 Text("sensitive")
+                                    .foregroundColor(.red) +
+                                 Text(" information in the chat.")
                                     .foregroundColor(.secondary)
-                                    .font(.system(size: 11, weight: .semibold))
+                                )
+                                    
+                                    .font(.system(size: 11, weight: .bold))
                                     .padding(.horizontal, 15)
                                     .padding(.bottom, 15)
                             
